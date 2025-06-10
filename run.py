5814b69597578ee998ae4a26589af3ba4134bb88
VilaNet710 <jordanvila128@gmail.com>
2025-06-10 19:45:21 -0400

    Actualización en run.py

--- run.py
+++ run.py
@@ -29,5 +29,5 @@ def home():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Crea las tablas en la base de datos si no existen
-
+## ucaaaaaaa
     app.run(debug=True)
